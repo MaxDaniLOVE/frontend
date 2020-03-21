b43b54309c10b7e077baf2257366c4f8175e1b64
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-03-22 00:36:06 +0300

    feat: add modal window

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 
 import './PlaceItem.scss';
 
@@ -9,30 +10,48 @@ const PlaceItem = ({id, image, title, description, address, creatorId, coordinat
     width: '540px',
     height: '600px'
   }
+  const [showMap, setShowMap] = useState(false);
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+  console.log(address)
   return(
-    <li className="place-item__card">
-      <Card style={cardStyles} className={'place-item__card'}>
-        <div className="place-item__image">
-          <img src={image} alt={title} />
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button className="danger" onClick={closeMapHandler}>X</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP!</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button className={'secondary'}>
-            VIEW ON MAP
-          </Button>
-          <Button className={'warning'} to={`/places/${id}`}>
-            EDIT
-          </Button>
-          <Button className={'danger'}>
-            DELETE
-          </Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item__card">
+        <Card style={cardStyles} className={'place-item__card'}>
+          <div className="place-item__image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button className={'secondary'} onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button className={'warning'} to={`/places/${id}`}>
+              EDIT
+            </Button>
+            <Button className={'danger'}>
+              DELETE
+            </Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   )
 }
 
