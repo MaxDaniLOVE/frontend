a158b46aa16362c4481e51968670968378bb9fea
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-03-22 09:57:37 +0300

    feat: add mark on map

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef
+} from 'react';
 import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibWF4ZGFuaWxvdiIsImEiOiJjazNkYTY2ZnEwdDZxM2NwOGJwN3hiMWZ6In0.EfNj3CDIoHMbo-pt3C_4oA';
-  
-  
-const Map = ({lng, lat}) => {
+
+const Map = ({
+  lng,
+  lat
+}) => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoibWF4ZGFuaWxvdiIsImEiOiJjazNkYTY2ZnEwdDZxM2NwOGJwN3hiMWZ6In0.EfNj3CDIoHMbo-pt3C_4oA';
-    const initializeMap = ({ setMap, mapContainer }) => {
+    const initializeMap = ({
+      setMap,
+      mapContainer
+    }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11",
@@ -22,13 +33,49 @@ const Map = ({lng, lat}) => {
       map.on("load", () => {
         setMap(map);
         map.resize();
+        map.loadImage("https://i.imgur.com/MK4NUzI.png", function (error, image) {
+          if (error) throw error;
+          map.addImage("custom-marker", image);
+          map.addLayer({
+            id: "markers",
+            type: "symbol",
+            source: {
+              type: "geojson",
+              data: {
+                type: 'FeatureCollection',
+                features: [{
+                  type: 'Feature',
+                  properties: {},
+                  geometry: {
+                    type: "Point",
+                    coordinates: [lng, lat]
+                  }
+                }]
+              }
+            },
+            layout: {
+              "icon-image": "custom-marker",
+            }
+          });
+        });
       });
     };
 
-    if (!map) initializeMap({ setMap, mapContainer });
-  }, [map]);
+    if (!map) initializeMap({
+      setMap,
+      mapContainer
+    });
+  }, [map, lat, lng]);
 
-  return <div ref={el => (mapContainer.current = el)} style={{height: '200px'}}/>;
+  return <div ref = {
+    el => (mapContainer.current = el)
+  }
+  style = {
+    {
+      height: '200px'
+    }
+  }
+  />;
 }
 
 export default Map;
